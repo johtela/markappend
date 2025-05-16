58876e7a5bed4684af37d4323db7a6bc5e4de334
johtela <tjohtela@gmail.com>
2025-05-16 17:18:08 +0300

    Structuring code

--- src/parser.ts
+++ src/parser.ts
@@ -1,10 +1,10 @@
 interface ParserState {
     input: string
-    result: string[]
     nextIndex: number
+    result: string[]
 }
 
-type Matcher = (state: ParserState, match: string) => number | void
+type Matcher = (state: ParserState, match: RegExpExecArray) => number | void
 
 interface Parser {
     regexp: string
@@ -12,7 +12,6 @@ interface Parser {
 }
 
 const notSpace = /[^ ]/
-const ecapedCodeSpan = / (.*) /
 
 function parser(regexp: string, matcher: Matcher) {
     return { regexp, matcher }
@@ -27,35 +26,40 @@ function regexpFor(parsers: Parser[]): RegExp {
 /**
  * Select parser that matches the current state and run it.
  */
-function select(regexp: RegExp, parsers: Parser[], state: ParserState) {
+function select(regexp: RegExp, parsers: Parser[], state: ParserState): 
+    boolean {
     regexp.lastIndex = state.nextIndex
     let match = regexp.exec(state.input)
     if (match && match.groups) {
-        for (let i = 0; i < parsers.length; i++) {
-            let res = match.groups[`g${i}`]
-            if (res) {
+        let parser = parsers.find((_, i) => match.groups![`g${i}`])
+        if (parser) {
+            let next = parser.matcher(state, match)
+            if (next) {
+                state.nextIndex = next
+                return true
             }
         }
     }
+    return false
 }
 /**
  * Define inline parsers in priority order. 
  */
 const inlineParsers = [
-    parser("`+", (state, match) => {
-        let cnt = match.length
-        let close = new RegExp(`(.*)\`{${cnt}}`, "yu")
-        close.lastIndex = state.nextIndex + cnt
-        let res = close.exec(state.input)
-        if (res) {
-            let escaped = ecapedCodeSpan.exec(res[1])
-            state.result.push(/*html*/`<code>${
-                escaped && notSpace.test(escaped[1]) ? 
-                    escaped[1] : res[1]
+    parser("(?<codeopen>`+)(?<codeinner> .+ |[^`]+)(?<codeclose>`+)", 
+        (state, match) => {
+            let { codeopen, codeinner, codeclose } = match.groups!
+            if (codeopen.length != codeclose.length) {
+                state.result.push(codeopen)
+                return match.index + codeopen.length
+            }
+            let cnt = codeinner.length
+            if (cnt > 2 && codeinner[0] == " " && codeinner[cnt - 1] == " ")
+                codeinner = codeinner.substring(1, cnt - 1)
+            state.result.push(/*html*/`<code>${codeinner.replaceAll("\n", " ")
                 }</code>`)
-            return close.lastIndex
-        }
-    })
+            return match.index + match[0].length
+        })
 ]
 const inlineRegexp = regexpFor(inlineParsers)
 
